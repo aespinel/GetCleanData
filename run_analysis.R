4003ae3b0d06fa3958e3d21921a67b8abb0e90cf
Alvaro Espinel <aespinel@gmail.com>
2014-12-19 19:41:11 -0500

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+setwd("C:/Users/Alvaro/datascience/Clean/Project/")
+#Read training data add two cols id 
+training = read.csv("UCI HAR Dataset/train/X_train.txt", sep="", header=FALSE)
+training[,562] = read.csv("UCI HAR Dataset/train/Y_train.txt", sep="", header=FALSE)
+training[,563] = read.csv("UCI HAR Dataset/train/subject_train.txt", sep="", header=FALSE)
+
+#read test data add two cols id
+testing = read.csv("UCI HAR Dataset/test/X_test.txt", sep="", header=FALSE)
+testing[,562] = read.csv("UCI HAR Dataset/test/Y_test.txt", sep="", header=FALSE)
+testing[,563] = read.csv("UCI HAR Dataset/test/subject_test.txt", sep="", header=FALSE)
+
+activityLabels = read.csv("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
+
+# Read features and make the feature names more clear for R with substitutions 
+features = read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
+features[,2] = gsub('-mean', 'Mean', features[,2])
+features[,2] = gsub('-std', 'Std', features[,2])
+features[,2] = gsub('[-()]', '', features[,2])
+
+# Merge training & test datasets 
+allData = rbind(training, testing)
+
+# Get only the data on mean and std. dev.
+colsWeWant <- grep(".*Mean.*|.*Std.*", features[,2])
+# First reduce the features table to what we want
+features <- features[colsWeWant,]
+# Now add the last two columns (subject and activity)
+colsWeWant <- c(colsWeWant, 562, 563)
+# Remove unwanted columns from allData
+allData <- allData[,colsWeWant]
+# Add the column names (features) to allData
+colnames(allData) <- c(features$V2, "Activity", "Subject")
+colnames(allData) <- tolower(colnames(allData))
+
+currentActivity = 1
+for (currentActivityLabel in activityLabels$V2) {
+  allData$activity <- gsub(currentActivity, currentActivityLabel, allData$activity)
+  currentActivity <- currentActivity + 1
+}
+
+allData$activity <- as.factor(allData$activity)
+allData$subject <- as.factor(allData$subject)
+
+tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
+# Remove the subject and activity column, because no mean of those has effect
+tidy[,90] = NULL
+tidy[,89] = NULL
+write.table(tidy, "tidy1.txt", sep="\t", row.names = FALSE )
